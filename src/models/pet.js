376753a1235b293eb458a198f59fad3775be7957
Aysia Brown <aysiaebrown@gmail.com>
2020-12-09 21:45:22 -0800

    started writing unit tests

--- src/models/pet.js
+++ src/models/pet.js
@@ -24,20 +24,25 @@ class PetModel {
     }
 
     update(id, obj) {
+        // console.log(obj)
         if(!id) { return null; }
         let pet = this.get(id);
         pet.name = obj.name || pet.name;
         pet.type = obj.type || pet.type;
         pet.age = obj.age ||  pet.age;
         this.db[this.grabIndex(id)] = pet;
-        return obj;
+        // console.log(obj)
+        // console.log(pet)
+        return pet;
 
     }
 
     delete(id) {
-        console.log('DEL id ', id);
+        // console.log('DEL id ', id);
         if(!id){return null; }
+        console.log('before ', this.db)
         this.db.splice(this.grabIndex(id),1);
+        console.log('after ', this.db)
         return null;
     }
 
