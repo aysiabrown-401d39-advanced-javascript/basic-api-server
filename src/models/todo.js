f422b1a38a7a512b90a7ce2476995d051ba35122
Aysia Brown <aysiaebrown@gmail.com>
2021-01-19 18:13:43 -0800

    updated todo model to match todo lab

--- src/models/todo.js
+++ src/models/todo.js
@@ -2,54 +2,53 @@
 
 class ToDoModel {
     constructor() {
+        this._id = 0;
+        this.complete;
         this.text;
-        this.completed;
-        this.assignee;
         this.difficulty;
-        this.id = 0;
+        this.assignee;
         this.db = [];
     }
 
-    get(id) {
-        if(id) {
-           return  this.db.find(record => record.id == id);
+    get(_id) {
+        if(_id) {
+           return  this.db.find(record => record._id == _id);
         } else {
             return this.db;
         }
     }
 
     create(obj) {  
-        obj.id = ++this.id;
+        obj._id = ++this._id;
         this.db.push(obj);
         return obj;
     }
 
-    update(id, obj) {
+    update(_id, obj) {
         // console.log(obj)
-        if(!id) { return null; }
-        let item = this.get(id);
+        if(!_id) { return null; }
+        let item = this.get(_id);
         item.text = obj.text || item.text;
         item.assignee = obj.assignee || item.assignee;
         item.difficulty = obj.difficulty ||  item.difficulty;
-        item.compeleted = obj.completed || item.completed;
-        this.db[this.grabIndex(id)] = item;
+        item.compeleted = obj.complete || item.complete;
+        this.db[this.grabIndex(_id)] = item;
         // console.log(obj)
         // console.log(pet)
         return item;
 
     }
 
-    delete(id) {
-        // console.log('DEL id ', id);
-        if(!id){return null; }
+    delete(_id) {
+        if(!_id){return null; }
         console.log('before ', this.db)
-        this.db.splice(this.grabIndex(id),1);
+        this.db.splice(this.grabIndex(_id),1);
         console.log('after ', this.db)
         return null;
     }
 
-    grabIndex(id) {
-        return this.db.findIndex(record => record.id === id);
+    grabIndex(_id) {
+        return this.db.findIndex(record => record._id === _id);
     }
 }
 
