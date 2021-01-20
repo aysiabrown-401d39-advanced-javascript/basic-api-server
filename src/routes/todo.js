9798f555591d1787f423929bd72e9ac8a6a6cc0e
Aysia Brown <aysiaebrown@gmail.com>
2021-01-19 19:57:10 -0800

    updated todo routes to send back proper data

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -33,8 +33,8 @@ function createTask(req, res) {
 function updateTask(req, res) {
     const obj = req.body;
     const id = req.params.id;
-    tasks.update(id, obj);
-    res.status(200).send('Task Complete!')
+    const update = tasks.update(id, obj);
+    res.status(200).json(update);
 }
 
 
